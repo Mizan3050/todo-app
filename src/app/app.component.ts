332b4b02ed64aa166f4383d845a139f0bbc0d529
Meezan <meezan@tradexa.co.in>
2021-05-23 19:37:48 +0530

    todo implemented with local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit{
+  
+  taskList: string[] = [];
+
+  inputTask = new FormControl('');
+  ngOnInit(): void {
+    const tasks: string[] = JSON.parse(localStorage.getItem('tasks') || '{}');
+    if(tasks.length>0) {
+      this.taskList.push(...tasks);
+    }
+  }
+
+  addTask(){
+    this.taskList.push(this.inputTask.value);
+    this.inputTask.reset();
+    localStorage.setItem('tasks', JSON.stringify(this.taskList))
+  }
+
+  deleteTask(i: number){
+    this.taskList.splice(i,1);
+    localStorage.setItem('tasks', JSON.stringify(this.taskList))
+  }
 }
